b76e336ee35920f005a48902bb76c04d9acd5042
Ikra Yasmina <ikrayasmina57@gmail.com>
2025-04-28 12:52:00 +0700

    Update _App.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,16 @@
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import { auth } from "../lib/firebaseConfig";
+import { useState, useEffect } from "react";
 import "../styles/globals.css";
 import "leaflet/dist/leaflet.css";
 import "leaflet-draw/dist/leaflet.draw.css";
 
 export default function MyApp({ Component, pageProps }) {
-  const router = useRouter();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      const isAuthPage = router.pathname.startsWith("/auth");
+    setLoading(false); // Set loading false setelah komponen dimuat
+  }, []);
 
-      if (user) {
-        // Jika sudah login dan berada di halaman login/register, arahkan ke dashboard
-        if (isAuthPage) router.push("/dashboard");
-      } else {
-        // Jika belum login dan bukan di halaman auth, redirect ke login
-        if (!isAuthPage) router.push("/auth/login");
-      }
-
-      setLoading(false);
-    });
-
-    return () => unsubscribe(); // Bersihkan listener saat unmount
-  }, [router]);
-
-  if (loading) return <div className="h-screen flex items-center justify-center text-xl">Loading...</div>;
+  if (loading) return <p>Loading...</p>;
 
   return <Component {...pageProps} />;
 }
